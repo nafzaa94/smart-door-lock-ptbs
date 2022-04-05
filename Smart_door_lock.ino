fcd05c19256319e201614d44d996c1ed029979b2
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-04-05 22:44:44 +0800

    full coding

---
+++ Smart_door_lock.ino
@@ -0,0 +1,102 @@
+#include <Adafruit_MLX90614.h>
+#include <Servo.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+#define echoPin 2 // pin echo
+#define trigPin 3 // pin trig
+
+Servo myservo;
+Servo myservo2;
+
+int temp = 0;
+double tempDisplay = 0;
+
+long duration;
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);
+  myservo2.attach(10);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  lcd.begin(); //kalu error tukar lcd.init();
+  lcd.backlight();
+
+  if (!mlx.begin()) {
+    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    while (1);
+  };
+
+  Serial.print("Emissivity = "); 
+  Serial.println(mlx.readEmissivity());
+
+  myservo.write(0);
+  myservo2.write(0);
+
+}
+
+void loop() {
+  Serial.print ("Temp = ");
+  Serial.println(mlx.readObjectTempC());
+  ultrasonic();
+
+  temp = mlx.readObjectTempC();
+  tempDisplay = mlx.readObjectTempC();
+
+  Lcd();
+
+  // utk temp
+  if (temp <= 37){
+    myservo.write(90);
+    lcd.setCursor(0, 4);
+    lcd.print("       ");
+    lcd.setCursor(0, 4);
+    lcd.print("OPEN");
+    delay(5000); // 5 sec
+    myservo.write(0);
+    }
+  else {
+    lcd.setCursor(0, 4);
+    lcd.print("       ");
+    lcd.setCursor(0, 4);
+    lcd.print("CLOSE");
+    myservo.write(0);
+    delay(1000);
+    }
+
+  // utk ultrasonic
+  if (distance <= 7){
+    myservo2.write(90);
+    delay(1000);
+    myservo2.write(0);
+    }
+
+}
+
+void ultrasonic(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  }
+
+void Lcd(){
+  lcd.setCursor(0, 4);
+  lcd.print("       ");
+  lcd.setCursor(0, 4);
+  lcd.print("WELCOME");
+  lcd.setCursor(1, 1);
+  lcd.print("Temp = ");
+  lcd.setCursor(1, 8);
+  lcd.print(tempDisplay, 1);
+  }
